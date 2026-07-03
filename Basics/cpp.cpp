c27677f0efd99b44fb331ca5fcb4a9e5914ac01a
Rohit Barhate <rohit.barhate28@gmail.com>
2026-07-03 13:30:54 +0100

    Sub 2

--- Basics/cpp.cpp
+++ Basics/cpp.cpp
@@ -27,3 +27,54 @@ class Solution {
         }
     };
 
+    // While Loop
+
+    // Given a digit d (0 to 9), find the sum of the first 50 positive integers (integers > 0) that end with digit d.
+    
+    
+    
+    // A number ends with digit d if its last digit is d.
+    
+    
+    // Example 1
+    
+    // Input: d = 1
+    
+    // Output: 12300
+    
+    // Explanation:
+    
+    // The first 50 positive integers ending with 1 are: 1, 11, 21, 31, ..., 491
+    
+    // Their sum is 12300.
+    
+    // Example 2
+    
+    // Input: d = 5
+    
+    // Output: 12500
+
+    class Solution {
+        public:
+        int whileLoop(int d) {
+            // Your code goes here  
+            int sum = 0; // sum for adding
+            int count = 0;
+    
+            int currentNumber;
+            if (d == 0) {
+                currentNumber = 10;
+                } 
+            else {
+                currentNumber = d;
+                }
+    
+            // int currentNumber = (d == 0) ? 10 : d; // alternative way to write above if else
+            while (count < 50) {
+                sum += currentNumber;
+                currentNumber += 10; // The next number ending in 'd' is always 10 away
+                count += 1;          // Increment our counter
+            }
+            return sum;
+        }
+    };
