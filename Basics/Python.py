335ff06c26216305e61a57468208a276b73ad3a9
Rohit Barhate <rohit.barhate28@gmail.com>
2026-07-07 02:18:17 +0100

    Sub 5, 6

--- Basics/Python.py
+++ Basics/Python.py
@@ -24,3 +24,68 @@ class Solution:
             for j in range(n):
                 print("*", end ="")
             print ()
+
+# Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+# *
+
+# **
+
+# ***
+
+# ****
+
+# *****
+
+# Print the pattern in the function given to you.
+
+class Solution:
+    def pattern2(self, n):
+        for i in range(n):
+            for j in range(i+1):
+                print("*", end ="")
+            print ()
+
+# # In Python, we can once again use string multiplication to avoid writing the inner loop entirely. 
+# # Since the number of stars precisely matches the row number, 
+# # we just multiply the string "*" by our current step in the loop.
+
+class Solution:
+    def pattern2(self, n):
+        for i in range(1, n+1):
+            print("*"*i)
+
+# Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+# 1
+
+# 12
+
+# 123
+
+# 1234
+
+# 12345
+
+
+
+# Print the pattern in the function given to you.
+
+class Solution:
+    def pattern3(self, n):
+        for i in range(1, n+1):
+            for j in range(1, i+1):
+                print(j, end ="")
+            print ()
+
+# # In Python, we can once again use string multiplication to avoid writing the inner loop entirely. 
+# # Since the number of stars precisely matches the row number, 
+# # we just multiply the string "*" by our current step in the loop.
+
+class Solution:
+    def pattern3(self, n):
+        for i in range(1, n+1):
+            print(str(i)*i)
+
--- Basics/cpp.cpp
+++ Basics/cpp.cpp
@@ -146,3 +146,63 @@ class Solution {
             }
         }
     };
+
+    // Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+    // *
+    
+    // **
+    
+    // ***
+    
+    // ****
+    
+    // *****
+    
+    
+    
+    // Print the pattern in the function given to you.
+
+    class Solution {
+        public:
+        void pattern2(int n) {
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j <= i; j++){
+                    cout<<"*";
+                }
+                cout<<endl;
+            }
+        }
+    };
+
+    // Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+    // 1
+    
+    // 12
+    
+    // 123
+    
+    // 1234
+    
+    // 12345
+    
+    
+    
+    // Print the pattern in the function given to you.
+
+    class Solution {
+        public:
+        void pattern3(int n) {
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j <= i; j++){
+                    cout<<j+1;
+                }
+                cout<<endl;
+            }
+        }
+    };
