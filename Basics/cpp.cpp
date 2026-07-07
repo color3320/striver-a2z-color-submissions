3b1ae620fdc3df7035eb1386265167c66d412640
Rohit Barhate <rohit.barhate28@gmail.com>
2026-07-07 02:35:33 +0100

    Sub 7, 8

--- Basics/cpp.cpp
+++ Basics/cpp.cpp
@@ -206,3 +206,35 @@ class Solution {
             }
         }
     };
+
+    // Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+    // 1
+    
+    // 22
+    
+    // 333
+    
+    // 4444
+    
+    // 55555
+    
+    
+    
+    // Print the pattern in the function given to you.
+    class Solution {
+        public:
+        void pattern4(int n) {
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j <= i; j++){
+                    cout<<i+1;
+                }
+                cout<<endl;
+            }
+        }
+    };
+
+
+    
